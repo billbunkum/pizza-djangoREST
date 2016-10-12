6334a6d2e6c52de92dc1771cd9a8f69af25353d4
billbunkum <william.richard.hurst@gmail.com>
2016-10-12 09:42:37 -0400

    search functionality using 'q'

--- pizzaMother/viewsets.py
+++ pizzaMother/viewsets.py
@@ -1,12 +1,18 @@
 from rest_framework import viewsets
 
-from .models import Pizza, Topping
 from .serializers import PizzaSerializer, ToppingSerializer
 from .models import Pizza, Topping
 
 class PizzaViewSet(viewsets.ModelViewSet):
     serializer_class = PizzaSerializer
     queryset = Pizza.objects.all()
+
+    def get_queryset(self):
+        query = self.request.query_params.get('q', None)
+        if query:
+            self.queryset = self.queryset.filter(name__icontains=query)
+
+        return self.queryset
 
 class ToppingViewSet(viewsets.ModelViewSet):
     serializer_class = ToppingSerializer
